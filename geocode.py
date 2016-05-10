53a63ff197f21571bf4d5af9b52bd0ff8e72af44
zhenxuan <nomorewzx@163.com>
2016-05-11 00:18:01 +0800

    Generate geocode dict for faster conversion.

--- geocode.py
+++ geocode.py
@@ -3,8 +3,18 @@ import csv
 
 GEOCODE = {}
 
-def main():
-	with open('')
+def genGeoCodeDict():
+	with open('geocode/geocoding-spot.csv') as csvfile:
+		spotGeocodeReader = csv.reader(csvfile)
+		for row in spotGeocodeReader:
+			GEOCODE[row[0]] = (row[1],row[2])
+	with open('geocode/geocoding.csv') as csvfile:
+		geocodeReader = csv.reader(csvfile)
+		for row in geocodeReader:
+			GEOCODE[row[0]] = (row[1],row[2])
+
+def convertGeoCode():
+	pass
 
 if __name__ == '__main__':
 	main()
